ca55416ad0afedb08b0d882bd5dc3057facaa3b1
Gordon Caister <gordoncaister@gmail.com>
2020-10-20 16:23:33 -0500

    began adding mailto button, made route selection dynamic

---
+++ frontend/MailTo/MailTo.js
@@ -0,0 +1,20 @@
+
+import React from "react"
+import {mailToContainer} from "../Styles/Styles"
+const MailTo = () => {
+    
+
+    return (
+        <div>
+            <div style={mailToContainer}>
+            <button>
+                Email Link
+            </button>
+        </div>
+            
+        </div>
+        
+        
+    )
+}
+export default MailTo;
